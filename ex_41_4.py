7ae3fc69f89a65c8497ed09baba82f5fec72e883
tonje <tonande02@gmail.com>
2021-06-14 14:09:50 +0200

    completed exercises 41_4 and 41_5

--- ex_41_4.py
+++ ex_41_4.py
@@ -1,11 +1,42 @@
 import time
 
-load_bar = list("|        |")
-pos = 1
+# load_bar = list("|        |")
+# pos = 1
 
-while pos < len(load_bar):
-    # list[pos] = "="
-    # pos += 1
-    # print(load_bar.join())
-    print(load_bar[pos])
-    pos += 1
+# while pos < len(load_bar):
+#     # list[pos] = "="
+#     # pos += 1
+#     # print(load_bar.join())
+#     print(load_bar[pos])
+#     pos += 1
+
+# print("|      |", end="", flush=True)
+# time.sleep(1)
+# print("|=>    |", end="", flush=True)
+# time.sleep(1)
+# print("|==>   |", end="", flush=True)
+# time.sleep(1)
+# print("|===>  |", end="", flush=True)
+# time.sleep(1)
+# print("|====> |", end="", flush=True)
+# time.sleep(1)
+# print("|=====>|", end="", flush=True)
+# time.sleep(1)
+# print("|======|", end="", flush=True)
+# time.sleep(1)
+
+
+print("|      |", flush=True, end='\r')
+time.sleep(1)
+print("|=>    |", flush=True, end='\r')
+time.sleep(1)
+print("|==>   |", flush=True, end='\r')
+time.sleep(1)
+print("|===>  |", flush=True, end='\r')
+time.sleep(1)
+print("|====> |", flush=True, end='\r')
+time.sleep(1)
+print("|=====>|", flush=True, end='\r')
+time.sleep(1)
+print("|======|", flush=True, end='\r')
+time.sleep(1)
---
+++ ex_41_5.py
@@ -0,0 +1,4 @@
+my_string = "This iS a HEllO worLd test"
+
+print(dir(my_string))
+print(help(my_string.join))
