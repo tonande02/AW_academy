ac3f67a1ddaf6519b87510ab26436261cf13ebf0
tonje <tonande02@gmail.com>
2021-06-14 11:20:21 +0200

    created and did exercise 41_3

---
+++ ex_41_3.py
@@ -0,0 +1,8 @@
+f_name = "Freddie"
+l_name = "Fredson"
+course = "AW Academy Data Engineer Intensive Training Program"
+nr_of_candidates = "11"
+
+print(f"My name is {f_name} {l_name}. ", end='')
+print(f"I'm currently taking the training course '{course}.' ", end='')
+print(f"There are {nr_of_candidates} candidates taking the course. ", end='')
