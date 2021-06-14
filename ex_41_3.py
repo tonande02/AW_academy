3ceb9aa9b775b861e318257ed97e31a8b3d030ea
tonje <tonande02@gmail.com>
2021-06-14 11:37:09 +0200

    edited 41_3 and created 41_4

--- ex_41_3.py
+++ ex_41_3.py
@@ -3,6 +3,6 @@ l_name = "Fredson"
 course = "AW Academy Data Engineer Intensive Training Program"
 nr_of_candidates = "11"
 
-print(f"My name is {f_name} {l_name}. ", end='')
-print(f"I'm currently taking the training course '{course}.' ", end='')
-print(f"There are {nr_of_candidates} candidates taking the course. ", end='')
+print(f"My name is {f_name} {l_name}.", end=' ')
+print(f"I'm currently taking the training course '{course}.'", end=' ')
+print(f"There are {nr_of_candidates} candidates taking the course.", end=' ')
